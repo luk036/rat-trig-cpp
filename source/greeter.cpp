187b55421595975817edf717ef0c5284917a00ee
Wai-Shing Luk <luk036@gmail.com>
2025-03-28 06:01:35 +0000

    remove greeter

--- source/greeter.cpp
+++ source/greeter.cpp
@@ -1,20 +1,2 @@
 #include <fmt/format.h>
-#include <rattrig/greeter.h>
-
-using namespace rattrig;
-
-RatTrig::RatTrig(std::string _name) : name(std::move(_name)) {}
-
-std::string RatTrig::greet(LanguageCode lang) const {
-    switch (lang) {
-        default:
-        case LanguageCode::EN:
-            return fmt::format("Hello, {}!", name);
-        case LanguageCode::DE:
-            return fmt::format("Hallo {}!", name);
-        case LanguageCode::ES:
-            return fmt::format("¡Hola {}!", name);
-        case LanguageCode::FR:
-            return fmt::format("Bonjour {}!", name);
-    }
-}
+#include <rattrig/trigonom.hpp>
