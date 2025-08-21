cbcd4ad7796aeb37cd0167e7510d40d6db74e9f2
Wai-Shing Luk <luk036@gmail.com>
2025-08-21 10:40:31 +0800

    Add more unit tests; changed xmake.lua

--- test/source/test_trigonom.cpp
+++ test/source/test_trigonom.cpp
@@ -2,6 +2,7 @@
 
 #include <fractions/fractions.hpp>  // for Fraction
 #include <ostream>                  // for Fraction, don't remove
+#include <boost/rational.hpp>
 #include <rattrig/trigonom.hpp>     // for archimedes
 
 using namespace rattrig;
@@ -27,9 +28,30 @@ TEST_CASE("Test archimedes (rational)") {
     CHECK_EQ(archimedes(q_1, q_2, q_3), fractions::Fraction<int>(23, 144));
 }
 
-// TEST_CASE("Test archimedes (rational)") {
-//   boost::rational<int> q_1(1, 2);
-//   boost::rational<int> q_2(1, 4);
-//   boost::rational<int> q_3(1, 6);
-//   CHECK_EQ(archimedes(q_1, q_2, q_3), boost::rational<int>(23, 144));
-// }
+TEST_CASE("Test archimedes (boost rational)") {
+   boost::rational<int> q_1(1, 2);
+   boost::rational<int> q_2(1, 4);
+   boost::rational<int> q_3(1, 6);
+   CHECK_EQ(archimedes(q_1, q_2, q_3), boost::rational<int>(23, 144));
+}
+
+TEST_CASE("Test archimedes (zero input)") {
+    int q_1 = 0;
+    int q_2 = 0;
+    int q_3 = 0;
+    CHECK_EQ(archimedes(q_1, q_2, q_3), 0);
+}
+
+TEST_CASE("Test archimedes (negative input)") {
+    int q_1 = -2;
+    int q_2 = -4;
+    int q_3 = -6;
+    CHECK_EQ(archimedes(q_1, q_2, q_3), 32);
+}
+
+TEST_CASE("Test archimedes (zero quadrea)") {
+    int q_1 = 1;
+    int q_2 = 1;
+    int q_3 = 2;
+    CHECK_EQ(archimedes(q_1, q_2, q_3), 4);
+}
